495941a3f12f1342ad8081b3fe8666b7778c42e8
洪布斯 <coderhbs@gmail.com>
2022-08-26 10:05:54 +0800

    chore: fix typo

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -6,7 +6,7 @@ const { config } = useConfig()
 </script>
 
 <template>
-  <nav class="absolute flex flex-col 3xl:w-80 2xl:w-72 h-full border-r border-gray-200 dark:border-gary-800">
+  <nav class="absolute flex flex-col 3xl:w-80 2xl:w-72 h-full border-r border-gray-200 dark:border-gray-800">
     <NavTitle />
     <div v-for="item in config.nav" :key="item.text">
       {{ item.text }}
