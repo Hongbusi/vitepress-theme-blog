b7b5dbf5c9c277d0ee1802140b46ec3d72edbab4
Hongbusi <coderhbs@gmail.com>
2024-06-04 00:12:08 +0800

    feat: add PostList component

--- packages/theme/src/components/HomePage.vue
+++ packages/theme/src/components/HomePage.vue
@@ -2,19 +2,22 @@
 import Profile from './widget/Profile.vue'
 import Categories from './widget/Categories.vue'
 import Tags from './widget/Tags.vue'
+import PostList from './PostList.vue'
 </script>
 
 <template>
   <div class="px-6 lg:px-8 py-8">
     <div class="max-w-[calc(var(--vp-layout-max-width)-64px)] mx-auto grid md:grid-cols-[12fr_25fr] xl:grid-cols-[23fr_54fr_23fr] gap-col-lg">
-      <div>
-        内容区
+      <div class="space-y-4">
+        <PostList />
       </div>
-      <aside class="order-0 md:order--1 space-y-4 pt-4 xl:pt0">
+
+      <aside class="order-0 md:order--1 space-y-4 pt-4 md:pt0">
         <Profile />
         <Categories />
         <Tags />
       </aside>
+
       <aside class="order-1 space-y-4 pt-4 xl:pt-0">
         <div class="base-card">
           最近文章
---
+++ packages/theme/src/components/PostList.vue
@@ -0,0 +1,27 @@
+<template>
+  <article v-for="(item, index) in 10" :key="index" class="base-card space-y-2">
+    <h2 class="text-lg font-bold">
+      这是一个文章标题啊
+    </h2>
+    <div class="flex space-x-2">
+      <div>
+        2024-1-1
+      </div>
+      <div>
+        分类：test
+      </div>
+      <div>
+        标签：1234
+      </div>
+    </div>
+    <p>这是一个文章描述啊。</p>
+    <div class="flex space-x-2">
+      <div>
+        400个字
+      </div>
+      <div>
+        需要5分钟
+      </div>
+    </div>
+  </article>
+</template>
